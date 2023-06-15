9b195b190257256e609ec16a19c5d7bf62e886a3
yojulab <otter.oh@gmail.com>
2023-06-15 16:14:09 +0900

    add somethings

---
+++ docs/survey/surveys.sql
@@ -0,0 +1,12 @@
+-- SELECT column_name(s)
+-- FROM table_name
+-- WHERE condition
+-- GROUP BY column_name(s)
+-- HAVING condition
+-- ORDER BY column_name(s);
+
+-- 설문자 가능 명단(가입 완료)
+-- 1. 홍길동, 2.장길산, 3.신사임당, ...
+SELECT *
+FROM respondents
+;
